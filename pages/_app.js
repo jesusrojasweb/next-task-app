56364dc741c3e53c68e925ba330e63baef29f126
Jesus Rojas <jesusrojas.frontend@gmail.com>
2022-07-27 18:14:59 -0400

    Initial styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,27 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import * as React from "react";
+import Head from "next/head";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseLine from "@mui/material/CssBaseline";
+import { CacheProvider } from "@emotion/react";
+import theme from "../src/theme";
+import createEmotionCache from "../src/createEmotionCache";
+
+const clientSideEmotionCache = createEmotionCache();
+
+function MyApp(props) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale-1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseLine />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
 
 export default MyApp;
---
+++ src/theme.js
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#17181f",
+    },
+    secondary: {
+      main: "#fa77a1",
+    },
+    error: {
+      main: "#ea4c89",
+    },
+  },
+});
+
+export default theme;
